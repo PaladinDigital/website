f0d8c9da5aa7dac1def22d0175640ef2ef0a9c18
David Craig <david@dcraig.dev>
2022-12-27 22:30:12 +0000

    Add navbar links and placeholder portfolio page.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -21,9 +21,10 @@ const Navbar = () => {
         </div>
       </div>
       <div className="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
-        {/* <a href="#" className="font-medium text-gray-500 hover:text-gray-900">Product</a>
+        <a href="/" className="font-medium text-gray-500 hover:text-gray-900">Home</a>
+        <a href="/privacy" className="font-medium text-gray-500 hover:text-gray-900">Privacy</a>
 
-        <a href="#" className="font-medium text-gray-500 hover:text-gray-900">Features</a>
+        {/* <a href="#" className="font-medium text-gray-500 hover:text-gray-900">Features</a>
 
         <a href="#" className="font-medium text-gray-500 hover:text-gray-900">Marketplace</a>
 
